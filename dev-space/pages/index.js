53763aa8053e7684aa63ef3802499b4d29367bf4
Ahmet Suhan Oka <ahmet.oka@yemeksepeti.com>
2021-05-27 16:41:14 +0300

    add header component and style

--- dev-space/pages/index.js
+++ dev-space/pages/index.js
@@ -1,3 +1,8 @@
+import Layout from "../components/Layout";
 export default function Home() {
-  return <h1>Hello world!</h1>;
+  return (
+    <Layout>
+      <h1>Hello! </h1>
+    </Layout>
+  );
 }
