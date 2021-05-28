f6b4cc5a8d8ff1e9a73b8ca17732134b2f78edf7
Ahmet Suhan Oka <ahmet.oka@yemeksepeti.com>
2021-05-28 23:27:49 +0300

    clean up get posts

--- dev-space/pages/index.js
+++ dev-space/pages/index.js
@@ -1,11 +1,8 @@
-import fs from "fs";
-import path from "path";
 import Link from "next/link";
-import matter from "gray-matter";
 import Layout from "@/components/Layout";
 import Post from "@/components/Post";
 
-import { sortByDate } from "@/utils/index";
+import { getPosts } from "@/lib/posts";
 
 export default function Home({ posts }) {
   //console.log(posts);
@@ -31,25 +28,9 @@ export default function Home({ posts }) {
 }
 
 export async function getStaticProps() {
-  const files = fs.readdirSync(path.join("posts"));
-
-  const posts = files.map((filename) => {
-    const slug = filename.replace(".md", "");
-
-    const markdownWithMeta = fs.readFileSync(
-      path.join("posts", filename),
-      "utf-8"
-    );
-    const { data: frontmatter } = matter(markdownWithMeta);
-    return {
-      slug,
-      frontmatter,
-    };
-  });
-
   return {
     props: {
-      posts: posts.sort(sortByDate).slice(0, 6),
+      posts: getPosts().slice(0, 6),
     },
   };
 }
