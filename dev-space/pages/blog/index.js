5236a19155b6a6da9a1d58955cdb2a519ccd6055
Ahmet Suhan Oka <ahmet.oka@yemeksepeti.com>
2021-05-28 21:40:20 +0300

    add pagination

--- dev-space/pages/blog/index.js
+++ dev-space/pages/blog/index.js
@@ -1,50 +1,5 @@
-import fs from "fs";
-import path from "path";
-import matter from "gray-matter";
-import Layout from "../../components/Layout";
-import Post from "../../components/Post";
+import { getStaticProps } from "./page/[page_index]";
+import BlogPage from "./page/[page_index]";
 
-import { sortByDate } from "../../utils";
-
-export default function BlogPage({ posts }) {
-  return (
-    <Layout>
-      <div className="blog-page">
-        <h1>Blog</h1>
-        <div className="blog-page-container">
-          {posts.map((post, index) => {
-            return (
-              <div key={index} className="post-area">
-                <Post post={post} />
-              </div>
-            );
-          })}
-        </div>
-      </div>
-    </Layout>
-  );
-}
-
-export async function getStaticProps() {
-  const files = fs.readdirSync(path.join("posts"));
-
-  const posts = files.map((filename) => {
-    const slug = filename.replace(".md", "");
-
-    const markdownWithMeta = fs.readFileSync(
-      path.join("posts", filename),
-      "utf-8"
-    );
-    const { data: frontmatter } = matter(markdownWithMeta);
-    return {
-      slug,
-      frontmatter,
-    };
-  });
-
-  return {
-    props: {
-      posts: posts.sort(sortByDate),
-    },
-  };
-}
+export { getStaticProps };
+export default BlogPage;
